3964caefba67a7ad4e27486aaa641913bd2eea2e
Kanathip-6406021611051 <6406021611051@fitm.kmutnb.ac.th>
2023-09-19 17:57:17 +0700

    save

---
+++ PGSqlCRUD.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const Sequelize = require('sequelize');
+const app = express();
+
+app.use(express,json());
+
+const dbUrl = 'postgres://webadmin:'
+
+const sequelize = new Sequelize(dbUrl);
