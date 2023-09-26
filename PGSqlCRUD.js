099e331e923ac4e3d6174da23e5938b6bd96ec1e
Kanathip-6406021611051 <6406021611051@fitm.kmutnb.ac.th>
2023-09-26 16:45:04 +0700

    save

--- PGSqlCRUD.js
+++ PGSqlCRUD.js
@@ -2,7 +2,7 @@ const express = require('express');
 const Sequelize = require('sequelize');
 const app = express();
 
-app.use(express,json());
+app.use(express.json());
 
 const dbUrl = 'postgres://webadmin:ALCedi13704@node50291-kanathip.proen.app.ruk-com,cloud:11569/Books'
 
